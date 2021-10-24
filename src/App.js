dcc10e38df9e624bce604a1a3f664167e364b0e4
Juraj Bedej <xbedej@stuba.sk>
2021-10-24 11:45:39 +0200

    HUH?

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import NotificationWidget from './components/NotificationWidget/NotificationWidg
 import { useEffect } from 'react';
 import React from 'react';
 import axios from 'axios';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
 
 const defaultZoom = 12
 const defaultCenter = {
   lat: 48.148598,
   lng: 17.107748
 }
+
+const data = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+  { name: 'Group C', value: 300 },
+  { name: 'Group D', value: 200 },
+];
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 function App() {
   const [districts, setDistricts] = React.useState([
@@ -136,6 +146,25 @@ function App() {
             <NotificationWidget alerts={getBins()} part={selected} />
           </div>
         </div>
+      </div>
+      <div>
+        <h2 style = {{textAlign: 'center'}}>Data visualization</h2>
+        <PieChart width={800} height={400}>
+        <Pie
+          data={data}
+          cx={120}
+          cy={200}
+          innerRadius={60}
+          outerRadius={80}
+          fill="#8884d8"
+          paddingAngle={5}
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
       </div>
     </div>
   );
